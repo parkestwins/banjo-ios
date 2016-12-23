784bf33d71fa0dbc0543eb4b7387c888bd95687e
Jarrod Parkes <jarrod@udacity.com>
2016-12-23 09:25:42 -0600

    add realm frameworks and basic model

---
+++ Banjo/Models.swift
@@ -0,0 +1,28 @@
+//
+//  Models.swift
+//  Banjo
+//
+//  Created by Jarrod Parkes on 12/23/16.
+//  Copyright © 2016 ParkesTwins. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+// MARK: Models
+
+final class Platform: Object {
+    dynamic var title = ""
+    dynamic var abbreviation = ""
+    dynamic var id = ""
+    let items = List<Game>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Game: Object {
+    dynamic var title = ""
+    dynamic var publisher = ""
+}
--- Banjo/RealmTestVC.swift
+++ Banjo/RealmTestVC.swift
@@ -8,6 +8,6 @@
 
 import UIKit
 
-class RealmTestVC: UIViewController {
-    
+class RealmTestVC: UITableViewController {
+
 }
